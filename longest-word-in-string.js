5bb7ede4353a9c5afcc5f31c2b30a6f14dc2ed04
andimiya <takamiya.andrea@gmail.com>
2016-10-22 11:14:04 -1000

    Add files via upload

---
+++ longest-word-in-string.js
@@ -0,0 +1,17 @@
+
+//FreeCodeCamp - Find the Longest Word in a String
+//https://www.freecodecamp.com/challenges/find-the-longest-word-in-a-string
+
+var newArray = [];
+
+function findLongestWord(str) {
+  var makeArray = /\s\s*/;
+  var nameList = str.split(makeArray);
+  for (var i = 0; i < nameList.length; i++) {
+    newArray.push(nameList[i].length);
+  }
+  newArray.sort();
+  console.log(newArray[newArray.length-1]);
+}
+
+findLongestWord("May the force be with you");
